976ce0f5aa21301286053c5a9f997adf57c61052
Joseph Rithin KA <josephrithin2004@gmail.com>
2026-07-23 14:49:01 +0530

    Create 027-remove-element.rs

---
+++ leetcode/027-remove-element.rs
@@ -0,0 +1,8 @@
+impl Solution {
+    pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
+        nums.retain(|&x| x!=val);
+        let mut k=nums.len();
+        let k:i32=k as i32;
+        return k
+    }
+}
